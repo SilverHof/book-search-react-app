604f1a4159ca1d8d6ecf2c3c5b265144e71cf1d7
SilverHof <63652935+SilverHof@users.noreply.github.com>
2023-01-10 13:02:54 +0300

    first commit

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React, { useEffect } from 'react';
+import './Home.scss'
+
+
+const Home = () => {
+
+
+    return (
+        <>
+            Its Home Page
+        </>
+    );
+};
+
+
+export default Home;
